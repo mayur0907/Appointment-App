1a539f7cd6575db29fd2b22d342df98ed1ef49e4
mayur0907 <121195884+mayur0907@users.noreply.github.com>
2023-04-15 14:04:22 +0530

    Delete database.js

--- database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('appointment','root','mayur123@#', {
-    dialect: 'mysql',
-    host: 'localhost'
-})
-
-module.exports = sequelize;
